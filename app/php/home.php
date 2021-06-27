6cff2c2cef1297b77ae8569a571a8c7d8231828a
MINI-MONSTER <hackuna885@gmail.com>
2021-06-27 16:45:48 -0500

    Control de Celulares

--- app/php/home.php
+++ app/php/home.php
@@ -9,9 +9,9 @@ require_once "seguridad.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- #################### Experimental borrar cache #################### -->
-    <!-- <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0"/> -->
+    <meta http-equiv="Expires" content="0"/>
     <!-- #################### Experimental borrar cache #################### -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
